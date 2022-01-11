97c05a96c453c7e5027c5ee33eb15df6c09ba409
avataromvatar <avataromvatar@yandex.ru>
2022-01-11 22:18:36 +0400

    init: start task reverse-int feat: add algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let tmp = "" + n;
+    const arr_char = tmp.split('');
+    let ret = "";
+    for (let index = arr_char.length - 1; index >= 0; index--) {
+        ret += arr_char[index];
+    }
+    return parseInt(ret);
 }
